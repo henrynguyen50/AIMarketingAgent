c30e12909078f1d64753a6bdc84380050d960376
henrynguyen50 <henryhuynguyen17@gmail.com>
2025-07-21 12:53:30 -0500

    testing api

---
+++ tweet.py
@@ -0,0 +1,42 @@
+import tweepy
+import os 
+import requests
+from google import genai
+from dotenv import load_dotenv
+load_dotenv()
+api_key =os.getenv("api_key")
+api_secret =os.getenv("api_secret")
+bearer_token= os.getenv("bearer_token")
+access_token =os.getenv("access_token")
+access_token_secret=os.getenv("access_token_secret")
+
+
+client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
+
+auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
+
+import requests
+
+api_url = "http://127.0.0.1:8000/gentweet"
+
+payload = {
+    "query": "Make a tweet for njpw"
+}
+
+try:
+    response = requests.post(api_url, json=payload)
+
+    if response.status_code == 200:
+        print("Success! API Response:")
+        # Print the JSON data returned by the API
+        print(response.json())
+    else:
+        print(f"Error: Received status code {response.status_code}")
+        print("Response:", response.text)
+
+except requests.exceptions.ConnectionError as e:
+    print(f"Connection Error: Could not connect to the server at {api_url}.")
+    print("Please make sure your FastAPI server is running.")
+
+#client.create_tweet(text="Wanna watch the G1 climax? This extension has all the needed links for wrestling chromewebstore.google.com/detail/the-stream-cleaner/khfgjcfkhmldbedicijbaekgkoniemmp")
